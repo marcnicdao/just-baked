8e28e70e485fc18daa70ee11d48a6c6510a3a1f2
Marc Nicdao <nicdaom466@gmail.com>
2020-06-15 10:46:23 -0700

    Added a conditional for displaying empty card

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -13,13 +13,16 @@ class CartSummary extends React.Component {
           key={item.cartItemId}/>
       );
     });
+    const footerText = totalPrice === 0
+      ? <h2>{'Your cart is empty'}</h2>
+      : <h4>{`Total: ${totalPrice}`}</h4>;
     return (
       <div className="container col-10 p-5">
         <div className="container-fluid details-main-container card">
           <span onClick={() => setView('catalog', {})}
             className="details-back-button">{'<Back to catalog'}</span>
           {itemList}
-          <h4>{`Total: ${totalPrice}`}</h4>
+          {footerText}
         </div>
       </div>
     );
